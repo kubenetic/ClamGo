e98a33f4f665e3c9483acfc0c8948e4ed94e6098
kubenetic <>
2025-10-25 10:12:39 +0200

    Implement FileStats interface

--- pkg/model/scan_request.go
+++ pkg/model/scan_request.go
@@ -1,26 +1,38 @@
 package model
 
 import (
-    "time"
+	"time"
 )
 
 type RequestFileMeta struct {
-    FileId string `json:"file_id"`
-    Name   string `json:"name"`
-    Sha256 string `json:"sha256"`
-    Size   int64  `json:"size"`
-    Path   string `json:"path"`
+	FileId string `json:"file_id"`
+	Name   string `json:"name"`
+	Sha256 string `json:"sha256"`
+	Size   int64  `json:"size"`
+	Path   string `json:"path"`
+}
+
+func (f *RequestFileMeta) GetFileId() string {
+	return f.FileId
+}
+
+func (f *RequestFileMeta) GetFileName() string {
+	return f.Name
+}
+
+func (f *RequestFileMeta) GetSha256() string {
+	return f.Sha256
 }
 
 type ScanRequest struct {
-    JobId     string            `json:"job_id"`
-    Attempts  int               `json:"attempts"`
-    Timestamp time.Time         `json:"timestamp"`
-    Files     []RequestFileMeta `json:"files"`
+	JobId     string            `json:"job_id"`
+	Attempts  int               `json:"attempts"`
+	Timestamp time.Time         `json:"timestamp"`
+	Files     []RequestFileMeta `json:"files"`
 }
 
 func (r *ScanRequest) IncrementAttempts() {
-    r.Attempts++
+	r.Attempts++
 }
 
 func (r *ScanRequest) GetFilePaths() []string {
